40930d56e2c7dfd06718ca6b2217e7eeb27369fa
Lucas Azzola <derflatulator@gmail.com>
2016-06-25 17:30:42 +1000

    Simplify service worker

--- sw.js
+++ sw.js
@@ -1,16 +1,14 @@
 
 importScripts('node_modules/sw-toolbox/sw-toolbox.js');
 
-toolbox.router.default = toolbox.fastest;
-
 toolbox.precache([
-    '/index.html', 
+    '/',
     '/build/minesweeper.js', 
     '/node_modules/lodash/lodash.js',
     '/node_modules/knockout/build/output/knockout-latest.js',
     '/img/sprite-small.png',
     '/img/sprite.png',
-    
+
     '/img/android-chrome-192x192.png',
     '/img/android-chrome-144x144.png',
     '/img/android-chrome-96x96.png',
@@ -19,58 +17,9 @@ toolbox.precache([
     '/img/android-chrome-36x36.png',
 ]);
 
-// based on:
-// https://developers.google.com/web/showcase/2015/service-workers-iowa#offline_google_analytics
+toolbox.router.default = toolbox.fastest;
 
-var DB_NAME = 'offline-analytics';
-var EXPIRATION_TIME_DELTA = 86400000;
+// Analytics
 var ORIGIN = /https?:\/\/((www|ssl)\.)?google-analytics\.com/;
-
-function replayQueuedAnalyticsRequests() {
-  caches.open(DB_NAME).then(function(cache) {
-    cache.keys().then(function (keys) {
-      keys.forEach(function(request, response) {
-        response.text().then(function (originalTimestamp) {
-          var timeDelta = Date.now() - +originalTimestamp;
-          var replayUrl = request.url + '&qt=' + timeDelta;
-          fetch(replayUrl).then(function(response) {
-            if (response.status >= 500) {
-                return Response.error();
-            }
-            db.delete(url);
-          }).catch(function(error) {
-            if (timeDelta > EXPIRATION_TIME_DELTA) {
-                cache.delete(request);
-            }
-          });
-        })
-      });
-    });
-  });
-}
-
-function queueFailedAnalyticsRequest(request) {
-  caches.open(DB_NAME).then(function(cache) {
-    cache.put(request, new Response(new Blob([Date.now()])));
-  });
-}
-
-function handleAnalyticsCollectionRequest(request) {
-  return fetch(request).then(function(response) {
-    if (response.status >= 500) {
-      return Response.error();
-    }
-    return response;
-  }).catch(function() {
-    queueFailedAnalyticsRequest(request);
-  });
-}
-
-toolbox.router.get('/collect',
-                   handleAnalyticsCollectionRequest,
-                   {origin: ORIGIN});
-toolbox.router.get('/analytics.js',
-                   toolbox.networkFirst,
-                   {origin: ORIGIN});
-
-replayQueuedAnalyticsRequests();
+toolbox.router.get('/collect', toolbox.networkOnly, { origin: ORIGIN });
+toolbox.router.get('/analytics.js', toolbox.networkFirst, { origin: ORIGIN });
